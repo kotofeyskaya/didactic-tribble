9e80a966483bb10dd15b611763085c9a0fa6e9c8
EkaterinaKhudoberdina <EkaterinaKhudoberdina@TeamCity>
2026-03-27 15:14:12 +0100

    Versioned settings configuration updated (TeamCity change in 'DSL test' project)

---
+++ .teamcity/settings.kts
@@ -0,0 +1,62 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2025.11"
+
+project {
+
+    vcsRoot(HttpsGithubComKotofeyskayaMavenSampleRefsHeadsMaster)
+
+    buildType(Build)
+    buildType(Build1)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(HttpsGithubComKotofeyskayaMavenSampleRefsHeadsMaster)
+    }
+})
+
+object Build1 : BuildType({
+    name = "Build 1"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+})
+
+object HttpsGithubComKotofeyskayaMavenSampleRefsHeadsMaster : GitVcsRoot({
+    name = "https://github.com/kotofeyskaya/maven-sample#refs/heads/master"
+    url = "https://github.com/kotofeyskaya/maven-sample"
+    branch = "refs/heads/master"
+    branchSpec = "refs/heads/*"
+    authMethod = password {
+        userName = "kotofeyskaya"
+        password = "credentialsJSON:05728551-a82b-4302-8689-aca90fcd7084"
+    }
+    param("pipelines.connectionId", "tc-cloud-github-connection")
+})
